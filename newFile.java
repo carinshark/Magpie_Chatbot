2e799d4e4fbddca50a2c25d4eef1177cf420bd3d
zev <150071728+zevalicious@users.noreply.github.com>
2024-10-28 12:45:51 -0700

    myFirstProject

---
+++ newFile.java
@@ -0,0 +1,58 @@
+
+public class newFile{
+    
+    public static void main(String[] args) {
+       String question = " bannana";
+
+        if (question.contains("pr")&& 
+         (question.contains("1")
+         ||question.contains("2")
+         ||question.contains("3")
+         ||question.contains("4")
+         ||question.contains("5")
+         ||question.contains("6")
+         ||question.contains("7")
+         ||question.contains("8")
+         ||question.contains("9"))){
+            boolean dave = true;
+                while(dave){
+                    int i = -1;
+                    i++;
+                    if (question.substring(i,i+1).equals("1")){
+                    }
+                    else if(question.substring(i,i+1).equals("2")){
+                    }
+                    else if(question.substring(i,i+1).equals("3")){
+                    }
+                    else if(question.substring(i,i+1).equals("4")){
+                    }
+                    else if(question.substring(i,i+1).equals("5")){
+                    }
+                    else if(question.substring(i,i+1).equals("6")){
+                    }
+                    else if(question.substring(i,i+1).equals("7")){
+                    }
+                    else if(question.substring(i,i+1).equals("8")){
+                    }
+                    else if(question.substring(i,i+1).equals("9")){
+                    }
+                    else if(question.substring(i,i+1).equals("0")){
+                    }
+
+                    else if(i >=30){
+                        dave = false;
+                    }
+                }
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
